2feb34495120012999ba9a7353f1d6c436638e0c
DapedGuardiola <muhammaddavid81@gmail.com>
2023-10-30 11:20:46 +0700

    Array Nilai Pertanyaan

--- jobsheet-9/ArrayNilai20.java
+++ jobsheet-9/ArrayNilai20.java
@@ -3,12 +3,16 @@ public class ArrayNilai20{
     public static void main(String [] args){
         Scanner input = new Scanner (System.in);
         int [] nilaiAkhir = new int [10];
-        for (int i = 0 ; i < 10 ; i++){
+        for (int i = 0 ; i < nilaiAkhir.length ; i++){
             System.out.println("Masukkan nilai akhir ke-"+i+" :");
             nilaiAkhir[i] = input.nextInt();
         }
-        for (int i = 0; i < 10 ; i++){
-            System.out.println("nilai akhir ke"+i+" adalah :"+nilaiAkhir[i]);
+        for (int i = 0; i < nilaiAkhir.length ; i++){
+            if(nilaiAkhir[i] > 70){
+                System.out.println("mahasiswa ke-"+i+" Dinyatakan lulus");
+            }else if (nilaiAkhir[i] < 70){
+                System.out.println("mahasiswa ke-"+i+" DInyatakan tidak lulus");
+            }
         }
     } 
 }
