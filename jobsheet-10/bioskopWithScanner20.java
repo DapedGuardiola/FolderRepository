e703f98d056469d20b0d0962398b975f60383845
DapedGuardiola <muhammaddavid81@gmail.com>
2023-12-05 13:10:10 +0700

    update jobhseet 10

--- jobsheet-10/bioskopWithScanner20.java
+++ jobsheet-10/bioskopWithScanner20.java
@@ -21,14 +21,21 @@ public class bioskopWithScanner20 {
     for (int i =0 ; i < penonton.length;){
         System.out.print("Masukkan nama penonton : ");
         nama = inputHuruf.nextLine();
+        do{
         System.out.print("Masukkan baris : ");
         baris = inputAngka.nextInt();
         System.out.print("Masukkan kolom : ");
-        kolom = inputAngka.nextInt();
+        kolom = inputAngka.nextInt(); 
+        if (baris>penonton.length && kolom > penonton[0].length)
+            System.out.print("Kursi tidak tersedia\n");     
+        }while(baris>penonton.length && kolom > penonton[0].length);
+        if (penonton[baris-1][kolom-1]!=null){
+            System.out.println("Kursi telah terisi");
+        }else{
         penonton [baris-1][kolom-1] = nama;
         System.out.println("Input penonton lainnya ? (y/n) ");
         chose = inputHuruf.nextLine();
-
+        
         if (chose.equalsIgnoreCase("y")){
             i++;
         }else if(chose.equalsIgnoreCase("n")){
@@ -36,25 +43,27 @@ public class bioskopWithScanner20 {
         }else {
             System.out.println("menu yang anda masukkan salah, mohon masukkan ulang");
             break;
-        } 
-
+        }
+    } 
     }
+
     } else if (pilihan == 2){
         System.out.println("=============================================");
         System.out.println("Anda berada di menu tampilan data penonton");
         System.out.println("Berikut daftar penonton di ...");
         System.out.println("=============================================");
-    if (penonton==null){
-        System.out.println("Kursi kosong");
-        System.out.println(System.out.println(penonton[0][0]));
-        System.out.println(penonton[0][1]);
-        System.out.println(penonton[1][0]);
-        System.out.println(penonton[1][1]);
-        System.out.println(penonton[2][0]);
-        System.out.println(penonton[2][1]);
-        System.out.println(penonton[3][0]);
-        System.out.println(penonton[3][1]);   
-    }
+        for(int i=0;i<penonton.length;i++){
+            for(int j=0;j<penonton[0].length;j++){
+                if(penonton[i][j]==null){
+                    System.out.print("****");
+                    System.out.print(" ");
+                }else{
+                System.out.print(penonton[i][j]);
+                System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     
 
     }else if (pilihan == 3){
